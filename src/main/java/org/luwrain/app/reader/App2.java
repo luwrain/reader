a62446bdcb524d73ebad7b1dc93372134f808381
Michael Pozhidaev <msp@luwrain.org>
2020-04-09 17:22:23 +0700

    Cleaning App2

--- src/main/java/org/luwrain/app/reader/App2.java
+++ src/main/java/org/luwrain/app/reader/App2.java
@@ -1,12 +1,25 @@
+/*
+   Copyright 2012-2020 Michael Pozhidaev <msp@luwrain.org>
+   Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.app.reader;
 
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
 import java.net.*;
 import java.io.*;
-import java.nio.file.*;
 
 import org.luwrain.core.*;
 import org.luwrain.controls.*;
@@ -27,15 +40,9 @@ enum ParaStyle {
 	EACH_LINE,
 	INDENT};
 
-
     private BookContainer bookContainer = null;
         private AudioPlaying audioPlaying = null;
-    
-        private final LinkedList<HistoryItem> history = new LinkedList();
-    private UrlLoader.Result res = null;
-        private StoredProperties storedProps = null;
-        private Book.Section[] sections = new Book.Section[0];
-    private final ListUtils.FixedModel notesModel = new ListUtils.FixedModel();
+    private StoredProperties storedProps = null;
 
     App2()
     {
@@ -44,12 +51,11 @@ enum ParaStyle {
 
     @Override public boolean onAppInit()
     {
-	/*
-	final AudioPlaying a = new AudioPlaying(luwrain);
-		this.audioPlaying = a.isLoaded()?a:null;
-	*/
-	return false;
-    }
+	this.audioPlaying = new AudioPlaying(getLuwrain());
+	if (!audioPlaying.isLoaded())
+		this.audioPlaying = null;
+		return true;
+		    }
 
     boolean openInitial(URL url, String contentType)
     {
@@ -89,200 +95,6 @@ enum ParaStyle {
 	return true;
     }
 
-        boolean changeCharset(String newCharset)
-    {
-	NullCheck.notNull(newCharset, "newCharset");
-	/*
-	if (isInBookMode() || isBusy() || !hasDocument())
-	    return false;
-	final UrlLoader urlLoader;
-	try {
-	    urlLoader = new UrlLoader(luwrain, res.doc.getUrl());
-	}
-	catch(MalformedURLException e)
-	{
-	    luwrain.crash(e);
-	    return false;
-	}
-		if (storedProps == null)
-	    storedProps = new StoredProperties(luwrain.getRegistry(), res.doc.getUrl().toString());
-		storedProps.setCharset(newCharset);
-	urlLoader.setCharset(newCharset);
-	final ParaStyle paraStyle = translateParaStyle(storedProps.getParaStyle());
-	if (paraStyle != null)
-	    urlLoader.setTxtParaStyle(paraStyle);
-	task = createTask(urlLoader);
-	luwrain.executeBkg(task);
-	*/
-	return true;
-    }
-
-    boolean changeTextParaStyle(ParaStyle newParaStyle)
-    {
-	NullCheck.notNull(newParaStyle, "newParaStyle");
-	/*
-	if (isInBookMode() || isBusy() || !hasDocument())
-	    return false;
-	final UrlLoader urlLoader;
-	try {
-	    urlLoader = new UrlLoader(luwrain, res.doc.getUrl());
-	}
-	catch(MalformedURLException e)
-	{
-	    luwrain.crash(e);
-	    return false;
-	}
-	if (storedProps == null)
-	    storedProps = new StoredProperties(luwrain.getRegistry(), res.doc.getUrl().toString());
-	storedProps.setParaStyle(newParaStyle.toString());
-	urlLoader.setTxtParaStyle(newParaStyle);
-	if (!storedProps.getCharset().isEmpty())
-	    urlLoader.setCharset(storedProps.getCharset());
-	task = createTask(urlLoader);
-	luwrain.executeBkg(task);
-	*/
-	return true;
-    }
-
-
-
-    boolean jumpByHrefInNonBook(String href, int currentRowIndex)
-    {
-	NullCheck.notEmpty(href, "href");
-	/*
-	if (isInBookMode())
-	    throw new RuntimeException("May not be in book mode");
-	NullCheck.notNull(res.doc, "res.doc");
-	    if (isBusy())
-	    return false;
-		final URL url;
-	try {
-	    url = new URL(href);
-	}
-	catch(MalformedURLException e)
-	{
-	    luwrain.message(strings.badUrl() + href, Luwrain.MessageType.ERROR);
-	    return true;
-	}
-	history.add(new HistoryItem(res.doc));
-	if (!openInitial(url, ""/*, currentRowIndex))
-	    return false;
-	luwrain.message(strings.fetching() + " " + href, Luwrain.MessageType.NONE);
-	    */
-	return true;
-    }
-
-    boolean onPrevDoc()
-    {
-	/*
-	if (isBusy())
-	    return false;
-	if (history.isEmpty())
-	    return false;
-	final HistoryItem item = history.pollLast();
-	    res.doc = item.doc;
-successNotification.run();
-	return true;
-	*/
-	return true;
-    }
-
-    Document jumpByHrefInBook(String href, int lastPos, int newDesiredPos)
-    {
-	NullCheck.notEmpty(href, "href");
-	/*
-	if (!isInBookMode() || isBusy())
-	    return null;
-	final Document doc = res.book.getDocument(href);
-	if (doc == null)
-	    return null;
-	if (doc != res.doc)
-	{
-	    if (lastPos >= 0 && !history.isEmpty())
-		history.getLast().lastRowIndex = lastPos;
-	    history.add(new HistoryItem(doc));
-	}
-	res.doc = doc;
-	if (newDesiredPos >= 0)
-	    res.doc.setProperty(Document.DEFAULT_ITERATOR_INDEX_PROPERTY, "" + newDesiredPos);
-	return doc;
-	*/
-	return null;
-    }
-
-    /*
-    private FutureTask createTask(UrlLoader urlLoader)
-    {
-	NullCheck.notNull(urlLoader, "urlLoader");
-	return new FutureTask(()->{
-		try {
-		    final UrlLoader.Result r = urlLoader.load();
-		    if (r != null)
-			luwrain.runUiSafely(()->{
-				task = null; //the Strong mark that the work is done
-				    onNewLoadingRes(r);
-				    successNotification.run();
-			    });
-		}
-		catch(Throwable e)
-		{
-		    Log.error("reader", "unable to fetch:" + e.getClass().getName() + ":" + e.getMessage());
-		    final Properties props = new Properties();
-		    props.setProperty("url", urlLoader.requestedUrl.toString());
-		    props.setProperty("contentType", urlLoader.getContentType());
-		    props.setProperty("charset", urlLoader.getCharset());
-		    luwrain.runUiSafely(()->errorHandler.accept(props, e));
-		}
-	}, null);
-    }
-    */
-
-    /*
-        private void onNewLoadingRes(UrlLoader.Result newRes)
-    {
-	NullCheck.notNull(newRes, "newRes");
-	if (isInBookMode() && res.book != null)
-	    throw new RuntimeException("Cannot open the new book being in book mode");
-	this.res = newRes;
-	if (res.book != null)
-	{
-	    this.sections = res.book.getBookSections();
-	    res.doc = res.book.getStartingDocument();
-	    history.clear();
-	    newBookNotification.run();
-	}
-	NullCheck.notNull(res.doc, "res.doc");
-	if (res.doc.getProperty("url").matches("http://www\\.google\\.ru/search.*"))
-	    Visitor.walk(res.doc.getRoot(), new org.luwrain.app.reader.filters.GDotCom());
-	if (StoredProperties.hasProperties(luwrain.getRegistry(), res.doc.getUrl().toString ()))
-	{
-	    this.storedProps = new StoredProperties(luwrain.getRegistry(), res.doc.getUrl().toString());
-	    final int savedPosition = storedProps.getBookmarkPos();
-	if (savedPosition > 0)
-	    res.doc.setProperty(Document.DEFAULT_ITERATOR_INDEX_PROPERTY, "" + savedPosition);
-	}
-	res.doc.commit();
-    }
-    */
-
-
-    /*
-    boolean fillDocProperties(MutableLines lines)
-    {
-	NullCheck.notNull(lines, "lines");
-	if (history.isEmpty())
-	    return false;
-	final HistoryItem item = history.getLast();
-	lines.beginLinesTrans();
-	lines.addLine(strings.propertiesAreaUrl(item.url));
-	lines.addLine(strings.propertiesAreaContentType(item.contentType));
-	lines.addLine(strings.propertiesAreaFormat(item.format));
-	lines.addLine(strings.propertiesAreaCharset(item.charset));
-	lines.addLine("");
-	lines.endLinesTrans();
-	return true;
-    }
-    */
 
     /*
     boolean playAudio(ReaderArea area, String[] ids)
@@ -305,16 +117,6 @@ successNotification.run();
 	return audioPlaying.stop();
     }
     */
-
-    /*
-    private URL getNotesUrl()
-    {
-	if (isInBookMode())
-	    return res.book.getStartingDocument().getUrl();
-	return res.doc.getUrl();
-    }
-    */
-
 
     BookContainer getBookContainer()
     {
