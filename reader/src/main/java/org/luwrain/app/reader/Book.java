ed4d9fe45e1cd38b83ec83a843cea8ef21c84494
Michael Pozhidaev <msp@luwrain.org>
2026-07-03 18:24:04 +0700

    new files

---
+++ reader/src/main/java/org/luwrain/app/reader/Book.java
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+// Copyright 2012-2026 Michael Pozhidaev <msp@luwrain.org>
+
+package org.luwrain.app.reader;
+
+import java.util.*;
+import lombok.*;
+
+@Data
+public final class Book
+{
+    private String name;
+}
---
+++ reader/src/main/java/org/luwrain/app/reader/Note.java
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+// Copyright 2012-2026 Michael Pozhidaev <msp@luwrain.org>
+
+package org.luwrain.app.reader;
+
+import java.util.*;
+import lombok.*;
+
+@Data
+public final class Note
+{
+    static final String
+	BOOKMARK = "BOOKMARK";
+
+    
+    private String type;
+}
